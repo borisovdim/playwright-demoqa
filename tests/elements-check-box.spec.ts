78f62a939f8fd23d4c845cf05b828756362f572d
borisovdim <paragvay85@gmail.com>
2025-05-27 22:07:33 +0300

    first commit

---
+++ tests/elements-check-box.spec.ts
@@ -0,0 +1,35 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Check Box', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://demoqa.com/checkbox');
+  });
+
+  test('Message for selected check boxes', async ({ page }) => {
+    await page.locator('[title="Expand all"]').click();
+    await page.locator('[for="tree-node-notes"]').click();
+    await page.locator('[for="tree-node-react"]').click();
+    await page.locator('[for="tree-node-downloads"]').click();
+
+    const result = page.locator('#result');
+    const messages = ['notes', 'react', 'downloads', 'wordFile', 'excelFile'];
+    for (const message of messages) {
+      await expect(result).toContainText(message);
+    }
+  });
+
+  test('Check boxes checked - unchecked', async ({ page }) => {
+    await page.locator('[for="tree-node-home"]').click();
+    await page.getByRole('button', { name: 'Toggle' }).first().click();
+    const checkboxesChecked = await page.locator('input[type="checkbox"]').all();
+
+    for (const checkbox of checkboxesChecked) {
+      await expect(checkbox).toBeChecked();
+    }
+    await page.locator('[for="tree-node-home"]').click();
+    const checkboxesUnChecked = await page.locator('input[type="checkbox"]').all();
+    for (const checkbox of checkboxesUnChecked) {
+      await expect(checkbox).not.toBeChecked();
+    }
+  });
+});

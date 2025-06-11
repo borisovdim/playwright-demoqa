57262e2871f73a35dfb6930298049c433a32e02a
borisovdim <paragvay85@gmail.com>
2025-06-11 20:12:05 +0300

    added tests for autocomlete added tests for slider

---
+++ tests/widgets-auto-complete.spec.ts
@@ -0,0 +1,40 @@
+import { test, expect, Locator } from './fixtures/adBlocker';
+
+test.describe('Auto Complete', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/auto-complete');
+  });
+
+  test('Types multiple color names', async ({ page }) => {
+    const colors = ['Red', 'Green', 'Blue'];
+    const multiInput = page.locator('#autoCompleteMultipleContainer');
+
+    for (const color of colors) {
+      await fillInput({ page }, color, multiInput);
+    }
+
+    const values = await page.locator('.auto-complete__multi-value').all();
+
+    for (const [index, value] of values.entries()) {
+      await expect(value).toHaveText(colors[index]);
+    }
+  });
+
+  test('Type single color name', async ({ page }) => {
+    const colors = ['Indigo', 'Red', 'Yellow'];
+    const singleInput = page.locator('#autoCompleteSingleContainer');
+    const singleValue = page.locator('[class$="singleValue"]');
+
+    for (const color of colors) {
+      await fillInput({ page }, color, singleInput);
+      await expect(singleValue).toHaveText(color);
+    }
+  });
+
+  const fillInput = async ({ page }, color: string, inputSelector: Locator) => {
+    await inputSelector.click();
+    await inputSelector.pressSequentially(color);
+    await page.waitForTimeout(100);
+    await inputSelector.press('Enter');
+  };
+});
---
+++ tests/widgets-slider.spec.ts
@@ -0,0 +1,49 @@
+import { test, expect, Locator } from './fixtures/adBlocker';
+
+let slider, sliderValue, tooltip: Locator;
+
+test.describe('Slider', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/slider');
+    slider = page.locator('input[type="range"]');
+    sliderValue = page.locator('#sliderValue');
+    tooltip = page.locator('[class*="tooltip__label"]');
+  });
+
+  test('Set slider value by JS', async () => {
+    await setSliderValueByJS('2');
+    await expect(sliderValue).toHaveValue('2');
+  });
+
+  test('Set slider value by mouse event', async ({ page }) => {
+    await setSliderValueByMouseEvent({ page }, 57);
+    await slider.hover();
+    await expect(tooltip).toHaveText('57');
+  });
+
+  const setSliderValueByJS = async (value: string) => {
+    await slider.evaluate((el, value) => {
+      const input = el as HTMLInputElement;
+      input.value = value;
+      input.dispatchEvent(new Event('input', { bubbles: true }));
+      input.dispatchEvent(new Event('change', { bubbles: true }));
+      input.style.setProperty('--value', value);
+    }, value);
+
+    await sliderValue.evaluate((el, value) => {
+      const input = el as HTMLInputElement;
+      input.value = value;
+    }, value);
+  };
+
+  const setSliderValueByMouseEvent = async ({ page }, targetValue: number) => {
+    const sliderElement = await slider.boundingBox();
+    const min = 0;
+    const max = 100;
+
+    const ratio = (targetValue - min) / (max - min);
+    const x = sliderElement.x + sliderElement.width * ratio;
+    const y = sliderElement.y + sliderElement.height / 2;
+    await page.mouse.click(x, y);
+  };
+});

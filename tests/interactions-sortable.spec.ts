521cb0b4d69fcd229b5e2b103877d1d516b74270
borisovdim <paragvay85@gmail.com>
2025-06-13 22:58:30 +0300

    format code

--- tests/interactions-sortable.spec.ts
+++ tests/interactions-sortable.spec.ts
@@ -5,29 +5,29 @@ test.describe('Sortable', () => {
     await page.goto('/sortable');
   });
 
-test('Small modal', async ({ page }) => {
-  const sourceArr = ['One', 'Two', 'Three', 'Four', 'Five'];
+  test('Sort elements', async ({ page }) => {
+    const sourceArr = ['One', 'Two', 'Three', 'Four', 'Five'];
 
-  for (const element of sourceArr) {
-    await dragToItem({ page }, element);
-  }
+    for (const element of sourceArr) {
+      await dragToItem({ page }, element);
+    }
 
-  sourceArr.push('Six');
-  const reversedArr = [...sourceArr].reverse();
+    sourceArr.push('Six');
+    const reversedArr = [...sourceArr].reverse();
 
-  const listItems = await page.locator('#demo-tabpane-list .list-group-item').all();
+    const listItems = await page.locator('#demo-tabpane-list .list-group-item').all();
 
-  for (const [index, expectedText] of reversedArr.entries()) {
-    const actualText = await listItems[index].textContent();
-    expect(actualText).toBe(expectedText);
-  }
-});
+    for (const [index, expectedText] of reversedArr.entries()) {
+      const actualText = await listItems[index].textContent();
+      expect(actualText).toBe(expectedText);
+    }
+  });
 
-const dragToItem = async ({ page }, element: string) => {
-  const panel = page.locator('#demo-tabpane-list');
-  const source = panel.locator('.list-group-item', { hasText: element });
-  const target = panel.locator('.list-group-item', { hasText: 'Six' });
+  const dragToItem = async ({ page }, element: string) => {
+    const panel = page.locator('#demo-tabpane-list');
+    const source = panel.locator('.list-group-item', { hasText: element });
+    const target = panel.locator('.list-group-item', { hasText: 'Six' });
 
-  await source.dragTo(target);
-};
+    await source.dragTo(target);
+  };
 });
